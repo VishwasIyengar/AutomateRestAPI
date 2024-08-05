55b29660426f4728fd58ebb1a1cfbb488acf1cb0
VishwasIyengar <vishwassv1995@gmail.com>
2024-08-05 20:11:57 +0530

    modified

--- src/test/java/AutomationRestAPI/RestAssured/AmazonIPhonePurchaseTest.java
+++ src/test/java/AutomationRestAPI/RestAssured/AmazonIPhonePurchaseTest.java
@@ -1,119 +1,117 @@
 package AutomationRestAPI.RestAssured;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class AmazonIPhonePurchaseTest 
-{
-     WebDriver driver;
-     WebDriverWait wait;
-    
+public class AmazonIPhonePurchaseTest {
+    WebDriver driver;
+    WebDriverWait wait;
+
     @SuppressWarnings("deprecation")
-	@BeforeClass
-    public void setUp() 
-    {
+    @BeforeClass
+    public void setUp() {
         // Set up WebDriver
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    	driver.manage().deleteAllCookies();
-        
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));  // Increase wait time
     }
-        
 
     @Test
-    public void testIPhonePurchase() throws InterruptedException 
-    {
-    	// Search for iPhone
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]"));
+    public void testIPhonePurchase() throws InterruptedException {
+        // Search for iPhone
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='twotabsearchtextbox']")));
         searchBox.sendKeys("iPhone");
-        
-        WebElement searchButton = driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]"));
+
+        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='nav-search-submit-button']")));
         searchButton.click();
-        
-        WebElement secondExpensiveIPhone = driver.findElement(By.linkText("Apple iPhone 15 Plus (128 GB) - Black"));
+
+        WebElement secondExpensiveIPhone = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Apple iPhone 15 Plus (128 GB) - Black")));
         secondExpensiveIPhone.click();
-        
+
         Set<String> s = driver.getWindowHandles();
-        ArrayList ar = new ArrayList(s);
-        System.out.println(ar.get(0));
-        System.out.println(ar.get(1));
-        driver.switchTo().window((String)ar.get(1));
-        
-        WebElement addCard = driver.findElement(By.xpath("(//input[@id='add-to-cart-button'])[2]"));
+        ArrayList<String> ar = new ArrayList<>(s);
+        driver.switchTo().window(ar.get(1));
+
+        WebElement addCard = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@id='add-to-cart-button'])[2]")));
         addCard.click();
-        
-        WebElement proceedToCheckOut = driver.findElement(By.xpath("(//input[@aria-labelledby='attach-sidesheet-checkout-button-announce'])[1]"));
+
+        WebElement proceedToCheckOut = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@aria-labelledby='attach-sidesheet-checkout-button-announce'])[1]")));
         proceedToCheckOut.click();
-        
-        WebElement enterEmail = driver.findElement(By.xpath("//input[@id='ap_email_login']"));
+
+        // Wait for the page to load completely
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='ap_email_login']")));
+
+        WebElement enterEmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='ap_email_login']")));
         enterEmail.sendKeys("vishwassv1995@gmail.com");
-        
-        WebElement continueButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='submit']")));
         continueButton.click();
-        
-        WebElement enterPassword = driver.findElement(By.xpath("//input[@id='ap_password']"));
+
+        WebElement enterPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='ap_password']")));
         enterPassword.sendKeys("Vishu@2024");
-        
-        WebElement signInButton = driver.findElement(By.xpath("//input[@id='signInSubmit']"));
+
+        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='signInSubmit']")));
         signInButton.click();
-        
-        WebElement fullNameInput = driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressFullName'])[1]"));
+
+        WebElement fullNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressFullName'])[1]")));
         fullNameInput.sendKeys("Vishwas Iyenagr");
-        
-        WebElement mobileNumberInput = driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressPhoneNumber'])[1]"));
+
+        WebElement mobileNumberInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressPhoneNumber'])[1]")));
         mobileNumberInput.sendKeys("6361797204");
-        
-        WebElement postalCodeInput =  driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressPostalCode'])[1]"));
+
+        WebElement postalCodeInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressPostalCode'])[1]")));
         postalCodeInput.sendKeys("560072");
-        
-        WebElement flatHousenoBuildingCompanyApartmentInput =  driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressLine1'])[1]"));
+
+        WebElement flatHousenoBuildingCompanyApartmentInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressLine1'])[1]")));
         flatHousenoBuildingCompanyApartmentInput.sendKeys("#20/21 Sri KrishnaVenu");
-        
-        WebElement areaStreetSectorVillageInput = driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressLine2'])[1]"));
+
+        WebElement areaStreetSectorVillageInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressLine2'])[1]")));
         areaStreetSectorVillageInput.sendKeys("Maruthi Nagar 5th cross Chandra Layout");
-        
-        WebElement landMark = driver.findElement(By.xpath("(//input[@id='address-ui-widgets-landmark'])[1]"));	
+
+        WebElement landMark = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-landmark'])[1]")));
         landMark.sendKeys("Beside Deffodil Kid Center");
-        
-       WebElement City  = driver.findElement(By.xpath("(//input[@id='address-ui-widgets-enterAddressCity'])[1]"));
-       City.sendKeys("BENGALURU");
-        WebElement stateDropdown = driver.findElement(By.xpath("(//span[@role='button'])[2]"));
-        stateDropdown.click(); // Assuming it's a custom dropdown that opens on click  
-        WebElement karnatakaOption = driver.findElement(By.xpath("(//a[normalize-space()='KARNATAKA'])[1]"));
-        try 
-         {
-        	karnatakaOption.click();
-         } 
-         catch (Exception e) 
-         {          
-        	 JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-             jsExecutor.executeScript("arguments[0].scrollIntoView(true);", karnatakaOption);         
-         } 
+
+        WebElement City = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@id='address-ui-widgets-enterAddressCity'])[1]")));
+        City.sendKeys("BENGALURU");
+
+        WebElement stateDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@role='button'])[2]")));
+        stateDropdown.click();
+
+        WebElement karnatakaOption = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[normalize-space()='KARNATAKA'])[1]")));
+        try {
+            karnatakaOption.click();
+        } catch (Exception e) {
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            jsExecutor.executeScript("arguments[0].scrollIntoView(true);", karnatakaOption);
+            jsExecutor.executeScript("arguments[0].click();", karnatakaOption);
+        }
+
         Thread.sleep(5000);
     }
-      
-    
+
     @AfterClass
-    public void tearDown() 
-    {       
+    public void tearDown() {
         driver.quit();
-	}
+    }
+}
+
         
 	 
-}
+
--- src/test/java/AutomationRestAPI/RestAssured/BrowserWindowsTest.java
+++ src/test/java/AutomationRestAPI/RestAssured/BrowserWindowsTest.java
@@ -1,61 +1,82 @@
 package AutomationRestAPI.RestAssured;
 
-import java.util.Set; 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class BrowserWindowsTest 
-{
-     WebDriver driver;
-     String mainWindowHandle;
+import java.time.Duration;
+import java.util.Set;
+
+public class BrowserWindowsTest {
+
+    WebDriver driver;
+    String mainWindowHandle;
 
     @BeforeClass
-    public void setUp() 
-    {
+    public void setUp() {
         // Set the path to chromedriver executable
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://demoqa.com/browser-windows");
-        mainWindowHandle = driver.getWindowHandle();
         driver.manage().window().maximize();
+        driver.get("https://demoqa.com/browser-windows");
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        mainWindowHandle = driver.getWindowHandle();
     }
 
     @Test
-    public void testNewTab() throws InterruptedException 
-    {
-        // Click on the "New Tab" button
-        WebElement newTabButton = driver.findElement(By.id("tabButton"));
-        newTabButton.click();
+    public void testNewTab() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Click on the "New Tab" button when it is clickable
+        WebElement newTabButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("tabButton")));
+        try {
+            newTabButton.click();
+        } catch (ElementClickInterceptedException e) {
+            // Use JavaScript as a fallback
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", newTabButton);
+        }
 
         // Switch to the new tab
         Set<String> allWindowHandles = driver.getWindowHandles();
-        for (String handle : allWindowHandles) 
-        {
-            if (!handle.equals(mainWindowHandle)) 
-            {
+        for (String handle : allWindowHandles) {
+            if (!handle.equals(mainWindowHandle)) {
                 driver.switchTo().window(handle);
                 break;
             }
         }
 
-        // Assert the title of the new tab
-        String expectedTitle = "This is a sample page";
-        String actualTitle = driver.getTitle();
-        Assert.assertNotEquals(actualTitle, expectedTitle);
+        // Wait for the new tab to load content
+        WebElement heading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sampleHeading")));
+
+        // Assert the heading of the new tab
+        String expectedHeading = "This is a sample page";
+        String actualHeading = heading.getText();
+        Assert.assertEquals(actualHeading, expectedHeading);
+
         Thread.sleep(5000);
     }
-    
-	@AfterClass
-    public void tearDown() 
-	{
-        // Close the new tab
-        driver.close();
+
+    @AfterClass
+    public void tearDown() {
+        // Close all tabs except the main window
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String handle : allWindowHandles) {
+            if (!handle.equals(mainWindowHandle)) {
+                driver.switchTo().window(handle);
+                driver.close();
+            }
+        }
 
         // Switch back to the main window
         driver.switchTo().window(mainWindowHandle);
@@ -64,6 +85,7 @@ public class BrowserWindowsTest
         driver.quit();
     }
 }
+
 
 
 
