40d7431b88236354371d4db1d8ee65c46eef6e4a
VishwasIyengar <vishwassv1995@gmail.com>
2024-08-20 20:15:33 +0530

    modified codes

--- src/test/java/AutomationRestAPI/RestAssured/AutomateRTCET.java
+++ src/test/java/AutomationRestAPI/RestAssured/AutomateRTCET.java
@@ -1,10 +1,14 @@
 package AutomationRestAPI.RestAssured;
 
 import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -27,15 +31,35 @@ public class AutomateRTCET
         driver.manage().deleteAllCookies();
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));  // Increase wait time to 60 seconds
     }
+    
+    
+    
   
   @Test
   public void testAllLinks() throws InterruptedException 
   {
-	  WebElement scheduleCall = driver.findElement(By.xpath("//span[contains(text(),'Schedule a Call')]"));
-	  scheduleCall.click();
+	
+	        // Locate the 'Services' tab in the navigation bar
+	  WebElement readMoreButton = driver.findElement(By.xpath("//span[contains(text(),'Read More')]"));
+	  readMoreButton.click();
 	  
-	  WebElement aboutUs = driver.findElement(By.xpath("//a[normalize-space()='About Us']"));
-	  aboutUs.click();
+	  WebElement contactUsButton = driver.findElement(By.xpath("//span[contains(text(),'Contact Us')]"));
+	  contactUsButton.click();
+	  
+	  WebElement fullNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='form-field-name']")));
+      fullNameInput.sendKeys("Vishwas Iyenagr");
+      
+      WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='form-field-email']")));
+      emailInput.sendKeys("vishwassv1995@gmail.com");
+      
+      WebElement messageInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@id='form-field-message']")));
+      messageInput.sendKeys("regarding job openings");
+      
+      WebElement submitButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+      submitButton.click();
+         
+      WebElement aboutUs = driver.findElement(By.xpath("//a[normalize-space()='About Us']"));
+	  aboutUs.click();//a[normalize-space()='DevOps Automation Services']
 	  
 	  WebElement services = driver.findElement(By.xpath("//a[normalize-space()='Services']"));
 	  services.click();
@@ -51,7 +75,7 @@ public class AutomateRTCET
 	  
 	  WebElement applyNow = driver.findElement(By.xpath("//span[@class='cus-nav']"));
 	  applyNow.click();
-	
+	  
 	  
 	  
       Thread.sleep(5000);
